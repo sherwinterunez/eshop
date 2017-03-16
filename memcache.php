18f026f1ace94a3711a2aca417f4a10b485a3c99
Sherwin Terunez <sherwinterunez@Sherwins-MacBook-Air.local>
2017-03-17 00:21:55 +0800

    commit march 17, 2017 00:21

---
+++ memcache.php
@@ -0,0 +1,67 @@
+<?php
+/*
+*
+* Author: Sherwin R. Terunez
+* Contact: sherwinterunez@yahoo.com
+*
+* Date Created: February 23, 2011
+*
+* Description:
+*
+* Application entry point.
+*
+*/
+
+//define('ANNOUNCE', true);
+
+error_reporting(E_ALL);
+
+ini_set("max_execution_time", 300);
+
+define('APPLICATION_RUNNING', true);
+
+define('ABS_PATH', dirname(__FILE__) . '/');
+
+if(defined('ANNOUNCE')) {
+	echo "\n<!-- loaded: ".__FILE__." -->\n";
+}
+
+//define('INCLUDE_PATH', ABS_PATH . 'includes/');
+
+require_once(ABS_PATH.'includes/index.php');
+//require_once(ABS_PATH.'modules/index.php');
+
+/*require_once(INCLUDE_PATH.'config.inc.php');
+require_once(INCLUDE_PATH.'miscfunctions.inc.php');
+require_once(INCLUDE_PATH.'functions.inc.php');
+require_once(INCLUDE_PATH.'errors.inc.php');
+require_once(INCLUDE_PATH.'error.inc.php');
+require_once(INCLUDE_PATH.'db.inc.php');
+require_once(INCLUDE_PATH.'pdu.inc.php');
+require_once(INCLUDE_PATH.'pdufactory.inc.php');
+require_once(INCLUDE_PATH.'utf8.inc.php');
+require_once(INCLUDE_PATH.'sms.inc.php');
+require_once(INCLUDE_PATH.'userfuncs.inc.php');*/
+
+date_default_timezone_set('Asia/Manila');
+
+$memcache = new Memcache;
+
+if(!$memcache->connect('127.0.0.1', 11211, 5)) {
+  unset($memcache);
+}
+
+if(!empty($memcache)) {
+  $customer_id = 10;
+  $keyword = 'ESHOP RL AT10 09493621618';
+
+  $key = sha1($customer_id.$keyword);
+
+  if(!$memcache->add($key, time(), 5)) {
+    print_r(array('error'=>'key already exists!'));    
+  }
+} else {
+  print_r(array('error'=>'memcached'));
+}
+
+///
