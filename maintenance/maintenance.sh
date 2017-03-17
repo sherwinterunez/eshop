196ccd92b3375623b3e5d0fbd1635e52c1466fdf
Sherwin Terunez <sherwinterunez@Sherwins-MacBook-Air.local>
2017-03-17 23:32:11 +0800

    commit march 17, 2017 23:32

--- maintenance/maintenance.sh
+++ maintenance/maintenance.sh
@@ -1,4 +1,12 @@
 
+PGDUMP=`which pg_dump`
+TAR=`which tar`
 BACKUP_DIR=/srv/backup/
+CURDATE=`date +\%Y-\%m-\%d-\%H-\%M`
+DBNAME=sherwint_eshop
+IPADD=127.0.0.1
+USERNAME=sherwint_sherwin
+SQLFILE=$BACKUP_DIR$DBNAME"-"$CURDATE.sql
+TGZFILE=$BACKUP_DIR$DBNAME"-"$CURDATE.sql.tgz
 
-/usr/bin/pg_dump -h 127.0.0.1 -U sherwint_sherwin -c sherwint_eshop > $BACKUP_DIR"sherwint_eshop-"`date +\%Y-\%m-\%d-\%H-\%M`.sql
+$PGDUMP -h $IPADD -U $USERNAME -c $DBNAME > $SQLFILE
