7ede3961e92a33c97710ee1a08b9b42f4d2eaa6e
Sherwin Terunez <sherwin@terunez.com>
2017-05-05 16:07:50 +0800

    commit may 5, 2017 16:10

--- modules/app.load.mod.php
+++ modules/app.load.mod.php
@@ -2346,7 +2346,8 @@ if(!class_exists('APP_app_load')) {
 					}
 
 				} else
-				if(!empty($post['method'])&&($post['method']=='onrowselect'||$post['method']=='loadedit')) {
+				if(!empty($post['method'])&&($post['method']=='onrowselect'||$post['method']=='loadedit'||$post['method']=='loadapproved'||$post['method']=='loadmanually'||$post['method']=='loadcancelled'||$post['method']=='loadhold'||$post['method']=='loadsave'||$post['method']=='loadtransfer')) {
+				//if(!empty($post['method'])&&($post['method']=='onrowselect'||$post['method']=='loadedit')) {
 					if(!empty($post['rowid'])&&is_numeric($post['rowid'])&&$post['rowid']>0) {
 						if(!($result = $appdb->query("select * from tbl_loadtransaction where loadtransaction_id=".$post['rowid']))) {
 							json_encode_return(array('error_code'=>123,'error_message'=>'Error in SQL execution.<br />'.$appdb->lasterror,'$appdb->lasterror'=>$appdb->lasterror,'$appdb->queries'=>$appdb->queries));
