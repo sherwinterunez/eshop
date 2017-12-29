4bc32ba1e162ca5ce4995e3f307c73e0b20d60de
Sherwin Terunez <sherwin@terunez.com>
2017-12-29 13:40:03 +0800

    commit dec 29, 2017 23:00

---
+++ assets/signing/sign-message.js
@@ -0,0 +1,82 @@
+/*
+ * JavaScript client-side example using jsrsasign
+ */
+
+// #########################################################
+// #             WARNING   WARNING   WARNING               #
+// #########################################################
+// #                                                       #
+// # This file is intended for demonstration purposes      #
+// # only.                                                 #
+// #                                                       #
+// # It is the SOLE responsibility of YOU, the programmer  #
+// # to prevent against unauthorized access to any signing #
+// # functions.                                            #
+// #                                                       #
+// # Organizations that do not protect against un-         #
+// # authorized signing will be black-listed to prevent    #
+// # software piracy.                                      #
+// #                                                       #
+// # -QZ Industries, LLC                                   #
+// #                                                       #
+// #########################################################
+
+/**
+ * Depends:
+ *     - jsrsasign-latest-all-min.js
+ *     - qz-tray.js
+ *
+ * Steps:
+ *
+ *     1. Include jsrsasign 8.0.4 into your web page
+ *        <script src="https://cdn.rawgit.com/kjur/jsrsasign/c057d3447b194fa0a3fdcea110579454898e093d/jsrsasign-all-min.js"></script>
+ *
+ *     2. Include this script into your web page
+ *        <script src="path/to/sign-message.js"></script>
+ *
+ *     3. Remove any other references to setSignaturePromise
+ */
+var privateKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
+    "MIIEpQIBAAKCAQEAxePDxH2+BbHsiQNEpx67TYtnpBKpFXDeSX7LxTBQ1E9XNex7\n" +
+    "w7+G+O1eOlePm9KM4jBeZxbatXDUiOPNsfCZos4xbYw7pMShOWErdn8WTFFBVv9q\n" +
+    "YjkBF8a2TdsksvRG/0uhoGdTfksyPGaWen4MHINFqF7xJpiyYTawzt4rt/viibPm\n" +
+    "ceYv115rf/E7IKiWi/A53I5vfJGpcestURWxjpyK4LKI7O9QOQCKklry0DtxtoCf\n" +
+    "gtEfF6joox3YiBaYm3rLG+kzcLRU/G2rwahnGySXO7mhpFDToV3npPMfwj9mUm5A\n" +
+    "94EyOp3yS2zrjfmBU3qG31nNYV5NFPk0YOPqdwIDAQABAoIBAArNAK1j05BKAsgD\n" +
+    "pqacdcqotjJtVk82RtuqEQMlPPQplG/8BVFMzdgeVItIpizyFABwdWaZB7zpeKA0\n" +
+    "FtEEec46BNae1c6LcOVJ2W5xR01JDhDqherwg4/Bp8eeE2W1EX7zqNGxcgwtnINR\n" +
+    "chsjcBPKUm37KJxbrzjBHGteEvLKsx790L5YH1uquh3vwV/1AMPHmZ5Gsc5/CHZT\n" +
+    "XNuEL5ftcaR5VEcoEzauCjaSc+uCq5jTilb9256Vwe0rHm0tSvxD8g+BOl0i3YCz\n" +
+    "1bu31gbwy3vhuhXvlcSVIN0HB3owL2T4ueu/agsJTnMRAxQUYdMHQ7Q/q99oYZot\n" +
+    "dlgbEvkCgYEA6gkXDNLiX2cDe8HeC3KQHpKUWCWqEl1yGNtecme+bpqv23tdHE0i\n" +
+    "B+IRKQLhUfTC1ZkS/1dGtGx4tRDJO2T4Sn8i7cdcMUFfx9w/vY8V9lN1Vi12oTzx\n" +
+    "UU9ZRNtL0MCjFpq5GbbOUs5YUryZqAn9eg/8zMls7OyKtd7gymdmBJ0CgYEA2HY9\n" +
+    "ZZjw8QR+4RhU726osuPvEb7io+OXA+47xfERMDGdIcSX24U5ifw9ZfQyGKpVP1Oq\n" +
+    "zgHfrrLRsdLPdEAfpz6zcuA7SXDe2OA+pXHMNuWvOUckuwGysVQsLmgKwKQqnoDO\n" +
+    "WmPi6PsY7gqtUnd/pkJW9Lwm1qVU6fnUh6KBnSMCgYEAyOkWzZ54UVg46FQsz30A\n" +
+    "9g31pZpn3y4zVd10vhgLph3LXEsSSsp4rXXIL4HWdqn9XKr1YRjGjPsVwLLxXbi3\n" +
+    "YhS0opXjsjEiDYwpeAzO2NEayOlsjsyniZrp0q/D7SiCmVQoFUgW5YVY02YvLs+s\n" +
+    "t83jwtYkWVxTVABKz9dmAA0CgYEAiInU4pAdczotuli5wqnfma+zLlNk7JHzwRP1\n" +
+    "+j68Y9T308QJMfwQNly4ceYd73cJ/L8O35FJAg5jvTJHj4GfNcSUNuMAIqwitVSF\n" +
+    "z6A/e2VYKN4aaieBCuAYWlFgbvFevMps35FgISu6JvTvLTSZfUsdRJSmPwDc1hWz\n" +
+    "nLbB4VcCgYEAuEGj/Gt+fHhfDvTHeF66PodBq7W018dS1uaRoxfY4/IqecXzWBiN\n" +
+    "NVO2W66GImiUDlQFVrNt31mR0N49VHaj5fEE8TJAG/0TLme+1nyImUuSjmExOIvC\n" +
+    "vk1wRPEVo0IBqvEfCqXSD7cKq5UiLt3jfmhRiTOem+NhRNiA4Rv1WX4=\n" +
+    "-----END RSA PRIVATE KEY-----\n";
+
+qz.security.setSignaturePromise(function(toSign) {
+    return function(resolve, reject) {
+        try {
+            var pk = KEYUTIL.getKey(privateKey);
+            var sig = new KJUR.crypto.Signature({"alg": "SHA1withRSA"});
+            sig.init(pk); 
+            sig.updateString(toSign);
+            var hex = sig.sign();
+            console.log("DEBUG: \n\n" + stob64(hextorstr(hex)));
+            resolve(stob64(hextorstr(hex)));
+        } catch (err) {
+            console.error(err);
+            reject(err);
+        }
+    };
+});
---
+++ assets/signing/sign-message.py
@@ -0,0 +1,54 @@
+#
+# Python Django example for views.py
+# Echoes the signed message and exits
+#
+
+#########################################################
+#             WARNING   WARNING   WARNING               #
+#########################################################
+#                                                       #
+# This file is intended for demonstration purposes      #
+# only.                                                 #
+#                                                       #
+# It is the SOLE responsibility of YOU, the programmer  #
+# to prevent against unauthorized access to any signing #
+# functions.                                            #
+#                                                       #
+# Organizations that do not protect against un-         #
+# authorized signing will be black-listed to prevent    #
+# software piracy.                                      #
+#                                                       #
+# -QZ Industries, LLC                                   #
+#                                                       #
+#########################################################
+
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from django.http import HttpResponse, HttpResponseBadRequest
+
+def index(request):
+    if request.method == 'GET':
+        message = request.GET.get('request')
+    else:
+        message = request.POST.get('request')
+
+    if message == '' or message == None:
+        return HttpResponseBadRequest("Signing request needs 'request' parameter")
+
+    mykey = os.path.join(os.path.dirname(__file__), "private-key.pem")
+    mypass = 'S3cur3P@ssw0rd' # or mypass = None
+
+    # Load the private key
+    key = serialization.load_pem_private_key(
+        open(mykey).read(), mypass.encode('utf-8'), backend=default_backend()
+    )
+
+    # Create the signature
+    signature = key.sign(message.encode('utf-8'), padding.PKCS1v15(), hashes.SHA1())
+
+    # Echo the signature
+    return HttpResponse(base64.b64encode(signature), content_type="text/plain")
+
